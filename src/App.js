a6c985f51be2f2bb702d4ec419aba09dd770880a
Swoopchinsky <parkerswoop93@mail.ru>
2025-02-15 20:40:48 +0300

    project 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./commponents/Header";
+import Footer from "./commponents/Footer";
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <Header />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/commponents/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <span className='logo'>IT Art Community</span>
+            </div>
+            <div className='presentation'></div>
+
+        </header>
+    )
+}
