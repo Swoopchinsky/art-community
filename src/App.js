dbee9cbd131cabb7572643f8567a7f2d10473f51
Swoopchinsky <parkerswoop93@mail.ru>
2025-02-17 10:53:46 +0300

    update 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./commponents/Header";
 import Footer from "./commponents/Footer";
+import Info from "./commponents/Info";
 
 function App() {
   return (
     <div className='wrapper'>
       <Header />
+      <Info />
       <Footer />
 
     </div>
---
+++ src/commponents/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Info() {
+    return (
+        <info>
+            <div className='info'>
+                Разработка одностраничных сайтов
+            </div>
+            <div className='infoblock'></div>
+        </info>
+    )
+}
