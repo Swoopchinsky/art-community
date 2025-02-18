6cfa0ea0cbbeb6dc0b1d8131f88d4b66ffef4cf9
Swoopchinsky <parkerswoop93@mail.ru>
2025-02-18 11:48:49 +0300

    buttom

--- src/commponents/Header.js
+++ src/commponents/Header.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DiProlog } from "react-icons/di";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { GiGoose } from "react-icons/gi";
 
 export default function Header() {
+    let [nigaYo, setNigaYo] = useState(false)
+    let [nigaYo1, setNigaYo1] = useState(false)
+    let [nigaYo2, setNigaYo2] = useState(false)
     return (
         <header>
             <div>
                 <span className='logo'>IT Art Community</span>
             </div>
+            <BsFillPersonLinesFill onClick={() => setNigaYo(nigaYo = !nigaYo)} className={`pers ${nigaYo && 'active'}`} />
+            <DiProlog onClick={() => setNigaYo1(nigaYo1 = !nigaYo1)} className={`menu ${nigaYo1 && 'active'}`} />
+            <GiGoose onClick={() => setNigaYo2(nigaYo2 = !nigaYo2)} className={`goo ${nigaYo2 && 'active'}`} />
+
             <div className='presentation'></div>
 
         </header>
