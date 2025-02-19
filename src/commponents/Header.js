5bf488ded07eb16119f1b30f4ed75e37b1aad636
Swoopchinsky <parkerswoop93@mail.ru>
2025-02-19 12:32:12 +0300

    pravka 2

--- src/commponents/Header.js
+++ src/commponents/Header.js
@@ -13,8 +13,23 @@ export default function Header() {
                 <span className='logo'>IT Art Community</span>
             </div>
             <BsFillPersonLinesFill onClick={() => setNigaYo(nigaYo = !nigaYo)} className={`pers ${nigaYo && 'active'}`} />
+            {nigaYo && (
+                <div className='pers1'>
+                    salam
+                </div>
+            )}
             <DiProlog onClick={() => setNigaYo1(nigaYo1 = !nigaYo1)} className={`menu ${nigaYo1 && 'active'}`} />
+            {nigaYo1 && (
+                <div className='menu1'>
+                    shalom
+                </div>
+            )}
             <GiGoose onClick={() => setNigaYo2(nigaYo2 = !nigaYo2)} className={`goo ${nigaYo2 && 'active'}`} />
+            {nigaYo2 && (
+                <div className='goo1'>
+                    Гусь это не просто птица!
+                </div>
+            )}
 
             <div className='presentation'></div>
 
