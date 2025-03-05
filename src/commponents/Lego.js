95d4058307afc66d15747725fdc8d5e4af40dd5f
Swoopchinsky <parkerswoop93@mail.ru>
2025-03-05 14:58:33 +0300

    pravka 4.8.0

--- src/commponents/Lego.js
+++ src/commponents/Lego.js
@@ -4,6 +4,9 @@ export default function Lego() {
     let [nigaLe, setNigaLe] = useState(false)
     let [nigaLe1, setNigaLe1] = useState(false)
     let [nigaLe2, setNigaLe2] = useState(false)
+    let [nigaLe3, setNigaLe3] = useState(false)
+    let [nigaLe4, setNigaLe4] = useState(false)
+    let [nigaLe5, setNigaLe5] = useState(false)
 
 
     return (
@@ -20,7 +23,26 @@ export default function Lego() {
                                     <div className='text1'>Marty McFly</div>
                                     <button onClick={() => setNigaLe2(nigaLe2 = !nigaLe2)} className={`but1ep1 ${nigaLe2 && 'active'}`} >go to Doc Brown</button>
                                     {nigaLe2 && (
-                                        <div className='cadr2'>new story</div>
+                                        <div className='cadr2'>
+                                            <div className='text2'>new adventures</div>
+                                            <button onClick={() => setNigaLe3(nigaLe3 = !nigaLe3)} className={`but2ep1 ${nigaLe3 && 'active'}`} >roll</button>
+                                            {nigaLe3 && (
+                                                <div className='cadr3'>
+                                                    <button onClick={() => setNigaLe4(nigaLe4 = !nigaLe4)} className={`but3ep1 ${nigaLe4 && 'active'}`} >put up a skate</button>
+                                                    {nigaLe4 && (
+                                                        <div className='cadr4'>
+                                                            <div className='text4'>Dog where are you ?</div>
+                                                            <button onClick={() => setNigaLe5(nigaLe5 = !nigaLe5)} className={`but4ep1 ${nigaLe5 && 'active'}`} >call Doc Brown</button>
+                                                            {nigaLe5 && (
+                                                                <div className='cadr5'>
+                                                                    <div className='text5'>I'm here Marty</div>
+                                                                </div>
+                                                            )}
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            )}
+                                        </div>
                                     )}
                                 </div>
                             )}
